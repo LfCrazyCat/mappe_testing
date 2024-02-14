33c09fc57577556d70b1c3436d5f019dcd598179
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-02-14 14:11:01 +0100

    update

--- min-react-app/src/App.jsx
+++ min-react-app/src/App.jsx
@@ -2,11 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import resources from './ressurser';
 
+
+function App() {
+  return (
+    <div>
+      <h1>Ressurser</h1>
+      <ul>
+        {resources.map((resource, index) => (
+          <li key={index}>
+            <a href={resource.url} target="_blank" rel="noopener noreferrer">{resource.title}</a>
+          </li> 
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -16,13 +39,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Ressursarkiv</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+           <code>src/App.jsx</code> Ressursarkiv
         </p>
       </div>
       <p className="read-the-docs">
@@ -33,3 +56,5 @@ function App() {
 }
 
 export default App
+ 
+*/
